c665a451c300d0bdae3fdabdf835d690eba36c2d
Brian Jr <thaballa79@gmail.com>
2023-01-27 23:43:00 -0600

    refactors code

--- app/src/main/java/jr/brian/issaaiapp/model/local/Chat.kt
+++ app/src/main/java/jr/brian/issaaiapp/model/local/Chat.kt
@@ -1,3 +1,7 @@
 package jr.brian.issaaiapp.model.local
 
-data class Chat(val text: String, val sender: String, val timeStamp: String)
+data class Chat(
+    val text: String,
+    val sender: String,
+    val timeStamp: String
+)
